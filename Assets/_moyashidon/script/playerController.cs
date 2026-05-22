cb1bef655da7358c80f35de401d4e880be6715f1
moyasidonn <ap_2900018895323@outlook.jp>
2026-05-22 15:05:18 +0900

    球発射

--- Assets/_moyashidon/script/playerController.cs
+++ Assets/_moyashidon/script/playerController.cs
@@ -3,17 +3,30 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    InputAction move,shot;
+    Rigidbody rb;
     void Start()
     {
+        move = InputSystem.actions.FindAction("Move");
+        move.Enable();
 
+        shot = InputSystem.actions.FindAction("Shot");
+        shot.Enable();
+
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Keyboard.current.upArrowKey.isPressed && transform.position.y < 4.5f)
-        {
-            transform.Translate(0, 0.02f, 0);
-        }
+        Vector2 moveValue = move.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector3(moveValue.x * 5f, moveValue.y, 0f);
+            if (shot.triggered)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                Destroy(bullet, 5f);
+                Debug.Log("Shot!");
+            }
     }
 
 }
