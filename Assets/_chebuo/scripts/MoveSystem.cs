a7c03ef52670abe661c9c109cd291b0a3e6a3bcc
chebuo <bob.chebuo@gmail.com>
2026-05-29 14:00:01 +0900

    [add] セレクト画面に戻る機能

--- Assets/_chebuo/scripts/MoveSystem.cs
+++ Assets/_chebuo/scripts/MoveSystem.cs
@@ -2,13 +2,14 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Collections;
 
 partial struct MoveSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
         var deltaTime = SystemAPI.Time.DeltaTime;
         foreach (var (move, transform, entity) in 
         SystemAPI.Query<RefRO<Move>, RefRW<LocalTransform>>().WithEntityAccess())
